6ea741cd663b3084680a3394b050d577215d5a24
srajma <srimahalingam@protonmail.com>
2025-10-12 00:06:02 +0100

    update maps

---
+++ admin_rivers.py
@@ -0,0 +1,16 @@
+import xatra
+
+def add_flags(map: xatra.FlagMap):
+    map.AdminRivers(sources=["naturalearth", "overpass"])
+    map.CSS(r""".river {stroke-width: 5;}""")
+
+if __name__ == "__main__":
+    m = xatra.FlagMap()
+    m.BaseOption("OpenStreetMap", default=True)
+    m.BaseOption("Esri.WorldImagery")
+    m.BaseOption("OpenTopoMap")
+    m.BaseOption("Esri.WorldPhysical")
+    add_flags(m)
+    m.TitleBox("""Rivers.""")
+
+    m.show(out_json="docs/admin_rivers.json", out_html="docs/admin_rivers.html")
